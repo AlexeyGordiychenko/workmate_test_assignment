c5315c1eb4f2c12cd2d506ca976261e6863db9ef
elidacon <elidacon@student.21-school.ru>
2025-04-13 16:12:35 +0500

    Add args parsing and ability to handle multiple log files

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
+import argparse
 from collections import defaultdict
-from sys import argv
 
-with open(argv[1], "r") as f:
-    counts = defaultdict(lambda: defaultdict(int))
-    for line in f:
-        if "django.request" in line:
-            parts = line.split(" ")
-            level = parts[2]
-            endpoint = next((x for x in parts if x.startswith("/")), None)
-            counts[endpoint][level] += 1
 
-for endpoint in sorted(counts.keys()):
-    print(endpoint)
-    for level in counts[endpoint]:
-        print(f"  {level}: {counts[endpoint][level]}")
+def process_log_file(log_file):
+    with open(log_file, "r") as f:
+        counts = defaultdict(lambda: defaultdict(int))
+        for line in f:
+            if "django.request" in line:
+                parts = line.split(" ")
+                level = parts[2]
+                endpoint = next((x for x in parts if x.startswith("/")), None)
+                counts[endpoint][level] += 1
+    print(f"\nLog file: {log_file}")
+    for endpoint in sorted(counts.keys()):
+        print(endpoint)
+        for level in counts[endpoint]:
+            print(f"  {level}: {counts[endpoint][level]}")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Parse Django log files and count log levels per endpoint."
+    )
+    parser.add_argument("log_files", nargs="+", type=str, help="Log files to parse.")
+    parser.add_argument(
+        "--report", "-r", type=str, help="Type of report.", required=True
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    for log_file in args.log_files:
+        process_log_file(log_file)
+
+
+if __name__ == "__main__":
+    main()
